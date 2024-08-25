41137b8f9c20221b21c73d800f7314bcc6fa3170
Letitia Pinter <60904782+ghostpilot22@users.noreply.github.com>
2024-08-24 22:29:38 -0600

    first commit and also got week 13 all working i thnk

---
+++ src/main/java/pet/store/entity/Employee.java
@@ -0,0 +1,39 @@
+package pet.store.entity;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+@Entity
+@Data
+public class Employee 
+{
+	@Id 
+	@GeneratedValue
+	private int employeeId;
+	
+	@Column
+	private String employeeFirstName;
+	
+	@Column
+	private String employeeLastName;
+	
+	@Column
+	private String employeePhone;
+	
+	@Column
+	private String employeeJobTitle;
+	
+	@ManyToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "pet_store_id")
+	@EqualsAndHashCode.Exclude
+	@ToString.Exclude
+	private PetStore petStore;
+}
---
+++ src/main/java/pet/store/entity/PetStore.java
@@ -0,0 +1,55 @@
+package pet.store.entity;
+
+import java.util.Set;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+@Entity
+@Data
+public class PetStore 
+{
+	@Id 
+	@GeneratedValue
+	private int petStoreId;
+	
+	@Column
+	private String petStoreName;
+	
+	@Column
+	private String petStoreAddress;
+	
+	@Column
+	private String petStoreCity;
+	
+	@Column
+	private String petStoreState;
+	
+	@Column
+	private int petStoreZip;
+	
+	@Column
+	private String petStorePhone;
+	
+	@ManyToMany(cascade = CascadeType.PERSIST)
+	@JoinTable(name = "pet_store_customer", joinColumns = @JoinColumn(name = "pet_store_id"), inverseJoinColumns = @JoinColumn(name = "customer_id"))
+	@EqualsAndHashCode.Exclude
+	@ToString.Exclude
+	private Set<Customer> customers;
+	
+	@OneToMany(mappedBy = "petStore", cascade = CascadeType.ALL, orphanRemoval = true)
+	@EqualsAndHashCode.Exclude
+	@ToString.Exclude
+	private Set<Employee> employees;
+	
+}
