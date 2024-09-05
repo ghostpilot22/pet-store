a88e3a00ba789c35efc0de40246c7c7bd71b893a
Letitia Pinter <60904782+ghostpilot22@users.noreply.github.com>
2024-09-05 15:08:17 -0600

    fixed errors, POST working now

--- src/main/java/pet/store/entity/Customer.java
+++ src/main/java/pet/store/entity/Customer.java
@@ -1,5 +1,6 @@
 package pet.store.entity;
 
+import java.util.HashSet;
 import java.util.Set;
 
 import jakarta.persistence.CascadeType;
@@ -32,5 +33,5 @@ public class Customer
 	@ManyToMany(mappedBy = "customers", cascade = CascadeType.PERSIST)
 	@EqualsAndHashCode.Exclude
 	@ToString.Exclude
-	private Set<PetStore> petStores; 
+	private Set<PetStore> petStores = new HashSet<>(); 
 }
